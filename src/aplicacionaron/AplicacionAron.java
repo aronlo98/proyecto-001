323bd51b8b8f0b6c459f3a9b0204ab4ac2e1d276
ProfL3302 <ProfL3302@L33235.lab.aca>
2025-07-01 19:42:08 -0500

    commit por carlos

--- src/aplicacionaron/AplicacionAron.java
+++ src/aplicacionaron/AplicacionAron.java
@@ -16,6 +16,7 @@ public class AplicacionAron {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        //comentario
     }
     
 }
